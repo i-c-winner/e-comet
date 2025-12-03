9c15af6ec41f87bda301ed435a005591b13aa856
Belousov Dmitriy <d-belousov@hotmail.com>
2025-12-03 15:13:17 +0500

    feat(stats-grid.tsx): создаст состояние компонента

--- src/features/stats/stats-grid/stats-grid.tsx
+++ src/features/stats/stats-grid/stats-grid.tsx
@@ -1,5 +1,5 @@
 import { AgGridReact } from 'ag-grid-react';
-import { useEffect, useState } from 'react';
+import { use, useEffect, useState } from 'react';
 import { IStatItem } from '../../../types/stats.types';
 import { STATS_API } from '../../../api/stats.api';
 
@@ -19,11 +19,23 @@ import { generateLevelPath } from '../../../utils/getPath.ts';
 import { ServerSideRowModelModule } from 'ag-grid-enterprise';
 
 import './stats-grid.scss';
+import { types } from 'sass';
 const dates = Array.from({ length: 30 }, (_, i) => new Date(Date.now() - i * 24 * 60 * 60 * 1000).toISOString().split('T')[0]);
 
 export function StatsGrid() {
     // ----------------------- HOOKS --------------------
 
+    const [currentDate, setCurrentDate] = useState<{
+        articles: IStatItem[];
+        brands: IStatItem[];
+        suppliers: IStatItem[];
+        types: IStatItem[];
+    }>({
+        articles: [],
+        brands: [],
+        suppliers: [],
+        types: [],
+    });
     const [columnDefs, setColumnDefs] = useState<ColDef<IStatItem>[]>([]);
     const [searchParams] = useSearchParams();
     const metric = searchParams.get('metric') ?? Metrics.cost;
@@ -37,6 +49,7 @@ export function StatsGrid() {
         const path = generateLevelPath(event.node);
         console.log('Group opened level:', path.length, path);
     }
+    const worker = new Worker(new URL('../../../workers/upgrade-data.worker.ts', import.meta.url), { type: 'module' });
 
     // ------------ SERVER ------------
 
@@ -49,68 +62,58 @@ export function StatsGrid() {
                     success: boolean;
                     rows: any[];
                 }>((resolve) => {
-                    const worker = new Worker(new URL('../../../workers/upgrade-data.worker.ts', import.meta.url), { type: 'module' });
-
                     STATS_API.getFull().then((data) => {
                         worker.postMessage({ data, dates });
                     });
 
-                    worker.onmessage = (event) => {
-                        const { suppliers, brands, types, articles } = event.data;
+                    let rows: any[] = [];
 
-                        let rows: any[] = [];
+                    if (level === 0) {
+                        rows = currentDate.suppliers.map((s: any) => ({
+                            supplier: s.supplier,
+                            __isGroup: true,
+                        }));
+                    }
 
-                        if (level === 0) {
-                            rows = suppliers.map((s: any) => ({
-                                supplier: s.supplier,
+                    if (level === 1) {
+                        const supplier = request.groupKeys[0];
+
+                        rows = currentDate.brands
+                            .filter((b: any) => b.supplier === supplier)
+                            .map((b: any) => ({
+                                brand: b.brand,
+                                supplier,
                                 __isGroup: true,
                             }));
-                        }
+                    }
 
-                        if (level === 1) {
-                            const supplier = request.groupKeys[0];
+                    if (level === 2) {
+                        const supplier = request.groupKeys[0];
+                        const brand = request.groupKeys[1];
 
-                            rows = brands
-                                .filter((b: any) => b.supplier === supplier)
-                                .map((b: any) => ({
-                                    brand: b.brand,
-                                    supplier,
-                                    __isGroup: true,
-                                }));
-                        }
+                        rows = currentDate.types
+                            .filter((t: any) => t.supplier === supplier && t.brand === brand)
+                            .map((t: any) => ({
+                                type: t.type,
+                                supplier,
+                                brand,
+                                __isGroup: true,
+                            }));
+                    }
 
-                        if (level === 2) {
-                            const supplier = request.groupKeys[0];
-                            const brand = request.groupKeys[1];
+                    if (level === 3) {
+                        const supplier = request.groupKeys[0];
+                        const brand = request.groupKeys[1];
+                        const type = request.groupKeys[2];
+                        rows = currentDate.articles.filter((a: any) => a.supplier === supplier && a.brand === brand && a.type === type);
+                    }
 
-                            rows = types
-                                .filter((t: any) => t.supplier === supplier && t.brand === brand)
-                                .map((t: any) => ({
-                                    type: t.type,
-                                    supplier,
-                                    brand,
-                                    __isGroup: true,
-                                }));
-                        }
+                    const page = rows.slice(request.startRow, request.endRow);
 
-                        if (level === 3) {
-                            const supplier = request.groupKeys[0];
-                            const brand = request.groupKeys[1];
-                            const type = request.groupKeys[2];
-                            rows = articles.filter((a: any) => a.supplier === supplier && a.brand === brand && a.type === type);
-                        }
-
-                        const page = rows.slice(request.startRow, request.endRow);
-
-                        resolve({
-                            success: true,
-                            rows: page,
-                        });
-                    };
-
-                    worker.onerror = (error) => {
-                        console.error('Worker error:', error);
-                    };
+                    resolve({
+                        success: true,
+                        rows: page,
+                    });
                 });
             },
         };
@@ -159,6 +162,20 @@ export function StatsGrid() {
 
         setColumnDefs([...grouping, ...cols]);
     }, [metric]);
+
+    useEffect(() => {
+        STATS_API.getFull().then((data) => {
+            worker.postMessage({ data, dates });
+        });
+        worker.onmessage = (event) => {
+            const { suppliers, brands, types, articles } = event.data;
+            setCurrentDate({ suppliers, brands, types, articles });
+        };
+
+        worker.onerror = (error) => {
+            console.error('Worker error:', error);
+        };
+    }, []);
 
     // ------------ RENDER ------------
 
